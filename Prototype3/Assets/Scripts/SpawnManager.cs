c1c8e3e810c2a9d18a8a06cbda270efecbc445b1
Jimmy459 <142622669+Jimmy459@users.noreply.github.com>
2024-04-11 12:47:02 -0700

    Removed unused libraries. Removed Empty Methods. Serialize Public Properties. SpawnManager.cs

--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     //Prefab for the obstacle to spawn
-    public GameObject obstaclePrefab;
+    [SerializeField] private GameObject obstaclePrefab;
     //Spawn Position
-    private Vector3 spawnPos = new Vector3(25.0f, 0.0f, 0.0f);
+    [SerializeField] private Vector3 spawnPos = new Vector3(25.0f, 0.0f, 0.0f);
 
     //Spawn delay
-    private float spawnDelay = 2.0f;
+    [SerializeField] private float spawnDelay = 2.0f;
     //Spawn Rate
-    private float spawnRate = 2.0f;
+    [SerializeField] private float spawnRate = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +19,7 @@ public class SpawnManager : MonoBehaviour
         InvokeRepeating("SpawnObstacle", spawnDelay, spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void SpawnObstacle()
     {
